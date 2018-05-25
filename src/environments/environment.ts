5f7cb08bdb7b2c1f0255d9b69b15c247e504f969
近藤慎一 <shinichi@weseek.co.jp>
2018-05-26 01:15:51 +0900

    * 微修正

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://www.googleapis.com/fusiontables/v1/query',
+  baseUrl: 'https://www.googleapis.com/fusiontables/v2/query',
   apiKey: 'AIzaSyAw3agkZGXhtiKDuSoxtG4Km2v_LKRYifg',
   tableId: '1Ah_7TW-dskYsF3E1XTFvz3GchgiVQ8X7UBFk-PBn',
   headerClassName: 'development',
