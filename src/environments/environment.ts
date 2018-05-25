33670934bb6196effabbed77e47c351982ebb00b
近藤慎一 <shinichi@weseek.co.jp>
2018-05-26 00:22:15 +0900

    * 追加コミット

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,25 +5,5 @@
 
 export const environment = {
   production: false,
-  sisngUrl: 'https://ix-nautilus-lab.lab.mfeed.ad.jp',
-  apiBaseUrl: '/api/v2',
-  liveStatusUrl: 'https://knd-test-ixops1.lab.mfeed.ad.jp/dashboard-configgem/api',
-  pingerMapping: {
-    IX0: [
-      { name: 'knd-test-pin3', isDesignated: true, isPicug: false },
-      { name: 'knd-test-pin4', isDesignated: false, isPicug: false },
-      { name: 'knd-test-pin7', isDesignated: false, isPicug: false },
-      { name: 'knd-test-pin8', isDesignated: false, isPicug: false },
-      { name: 'knd-test-pin11', isDesignated: false, isPicug: false },
-      { name: 'knd-test-pin12', isDesignated: false, isPicug: false },
-    ]
-  },
-  mrtgUrlMapping: {
-    IX0: [
-      { host: 'knd-test-mrtg7.lab.mfeed.ad.jp', isMain: true },
-      { host: 'knd-test-mrtg8.lab.mfeed.ad.jp', isMain: false }
-    ]
-  },
-  mrtgBackupImageUrl: 'https://knd-test-ixops1.lab.mfeed.ad.jp/mrtg-backup',
   headerClassName: 'development',
 };
