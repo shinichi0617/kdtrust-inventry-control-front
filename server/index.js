b4357bfaf9aff744d7fcd55fdcdec54e5d6df304
近藤慎一 <shinichi@weseek.co.jp>
2018-05-27 08:13:11 +0900

    * 微修正

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(compression());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static('dist'));
+app.use(express.static(__dirname + '/../dist'));
 
 // Start the app by listening on the default
 // Heroku port
@@ -18,7 +18,7 @@ app.listen(port);
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname + '/../dist/index.html'));
 });
 
 console.log(`Server listening on ${port}`);
